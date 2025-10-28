b6eb0fb1a9c382ea6546a1f3f2ebcf174652dd12
khane28e <khane28e@gmail.com>
2025-10-28 10:58:42 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+item = input("what would you like to buy?")
+price = float(input("what's the price?"))
+quality = int(input("how many would you like to buy?"))
+total = price * quality
+
+print(f"you have bought {quality}!")
+print (f"your total will be {total}")
